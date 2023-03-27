e4c093132977b80097d0a3ea9b6c54b3c69a7d22
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2023-03-27 19:47:19 -0400

    Game more challenging and rewarding
    Added acceleration and gravity: Added variables to represent the bird's acceleration and the acceleration due to gravity, and updated the bird's velocity and position using these values in the main game loop. This made the bird's movement more realistic and added a challenge to the game.
    
    Added a score multiplier: Added a variable to keep track of the score multiplier, which increases every 2 points the player earns. This makes it more rewarding for the player to get a high score.
    
    Added speed increase: Added code to increase the game speed (i.e., the speed of the pipes) every 10 points the player earns. This added more challenge to the game as the player progresses.
    
    Added pausing: Added code to allow the player to pause the game by pressing the "Esc" key. This makes it more convenient for the player to take a break if needed.
    
    Added delay before game over screen: Added a delay before showing the game over screen after the player collides with a pipe. This gave the player a moment to see the bird's final position before the game ends.

--- FlappyBird.cc
+++ FlappyBird.cc
@@ -14,8 +14,10 @@ const char PIPE = '|';
 const char BLANK = ' ';
 
 void drawScreen(const int birdX, const int birdY, const int pipe1X, const int pipe2X, int score) {
+    
     // Display the current score
     std::cout << "Score: " << score << std::endl;
+
     // Draw the game screen
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
         for (int x = 0; x < SCREEN_WIDTH; x++) {
@@ -32,6 +34,7 @@ void drawScreen(const int birdX, const int birdY, const int pipe1X, const int pi
 }
 
 void gameOverScreen(int score) {
+
     // Display the game over message and the player's final score
     std::cout << "Game over!\nYour score is: " << score << std::endl;
     std::cout << "Press any key to exit...";
@@ -39,8 +42,10 @@ void gameOverScreen(int score) {
 }
 
 int main() {
+
     // Seed the random number generator
     srand(time(NULL));
+
     // Initialize variables for the bird's position and pipe's position
     int birdX = SCREEN_WIDTH / 2;
     int birdY = SCREEN_HEIGHT / 2;
@@ -50,34 +55,62 @@ int main() {
     int pipeGap = 3;
     int birdVelocity = 0;
     int score = 0;
+    int delayAfterCollision = 50; // Delay before game over screen appears (in milliseconds)
+    int speedIncreaseInterval = 10; // Increase game speed every 10 points
+    float birdPosY = birdY;
+    float birdVelY = 0;
+    float gravity = 0.1; // Acceleration due to gravity
+    float birdAccY = 0;
+    int scoreMultiplier = 1;
+    bool paused = false;
+
     // Main game loop
     while (true) {
+
         // Handle user input
         if (GetAsyncKeyState(VK_SPACE)) {
-            birdVelocity = -1;
+            if (!paused) {
+                birdAccY = -0.5; // Accelerate the bird upward
+            }
         }
+        if (GetAsyncKeyState(VK_ESCAPE)) {
+            paused = !paused; // Toggle pause state
+        }
+
         // Update game state
-        birdY += birdVelocity;
-        birdVelocity++;
-        pipe1X--;
-        pipe2X--;
-        if (pipe1X <= 0) {
-            pipe1X = SCREEN_WIDTH;
-            pipe2X = SCREEN_WIDTH + 4;
-            pipeY = rand() % (SCREEN_HEIGHT - pipeGap) + 1;
+        if (!paused) {
+            birdPosY += birdVelY; // Update bird position
+            birdVelY += birdAccY; // Update bird velocity
+            birdAccY += gravity; // Update bird acceleration
+            birdY = static_cast<int>(birdPosY); // Convert bird position to integer
+            pipe1X--;
+            pipe2X--;
+            if (pipe1X <= 0) {
+                pipe1X = SCREEN_WIDTH;
+                pipe2X = SCREEN_WIDTH + 4;
+                pipeY = rand() % (SCREEN_HEIGHT - pipeGap) + 1;
+            }
+            if ((birdX == pipe1X || birdX == pipe2X) && (birdY < pipeY || birdY > pipeY + pipeGap)) {
+                // Game over
+                Sleep(delayAfterCollision);
+                gameOverScreen(score);
+                break;
+            }
+
+            // Increment score if bird passes through pipes
+            if (birdX == pipe1X + 1) {
+                score += scoreMultiplier;
+                if (score % speedIncreaseInterval == 0) {
+                    pipe1X++; // Increase game speed
+                    pipe2X++;
+                }
+                if (score % 2 == 0) {
+                    scoreMultiplier++; // Increase score multiplier every 2 points
+                }
+            }
         }
-        if ((birdX == pipe1X || birdX == pipe2X) && (birdY < pipeY || birdY > pipeY + pipeGap)) {
-            // Game over
-            gameOverScreen(score);
-            break;
-        }
-        // Increment score if bird passes through pipes
-        if (birdX == pipe1X + 1) {
-            score++;
-        }
+
         // Draw the screen
         drawScreen(birdX, birdY, pipe1X, pipe2X, score);
         Sleep(100);
     }
-    return 0;
-}
